5774595173d4b146bd73861ac2272bd71b1c21c3
Ivan Pakhomov <ip1133@ya.ru>
2024-05-11 00:37:07 +0300

    feat: add css module & page not found

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import * as style from './NotFoundPage.module.css';
+
 function NotFoundPage() {
-  return <h1>404 Not Found</h1>;
+  return (
+    <div className={style.wrapper}>
+      <div className={style.wrapper_text}>
+        <h1>Page Not Found</h1>
+      </div>
+      <div className={style.wrapper_block}>
+        <h2>404</h2>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
