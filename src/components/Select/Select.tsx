fd6397dc03b5ee1bb0e22c00aba6eaf0f88eab46
HNY_Badger <markelov-02@list.ru>
2024-05-11 15:35:15 +0400

    feat: add Select component

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEventHandler } from 'react';
+import * as styles from './Select.module.css';
+
+export type Option = {
+  text: string;
+  value: string;
+  key: number;
+};
+
+type SelectProps = {
+  value: string;
+  onChange: ChangeEventHandler<HTMLSelectElement>;
+  label: string;
+  id: string;
+  options: Option[];
+};
+
+function Select({ value, onChange, id, label, options }: SelectProps) {
+  return (
+    <div className={styles.select_wrapper}>
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+      <select value={value} onChange={onChange} id={id} className={styles.select}>
+        {options.map((option) => (
+          <option key={option.key} className={styles.option} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
