00af80f2b5eddcb0b02a16485b102cd0b4d9645f
marinrika <119961838+marinrika@users.noreply.github.com>
2024-05-11 17:12:49 +0300

    Add Shop Features to Main Page (#20)
    * refactor: change folder structure
    
    * feat: add FeatureCard
    
    * feat: add ShopFeatures
    
    * feat: add adaptive css
    
    * fix: add main tag and fix a typo

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import ShopDescription from '../../components/MainPage/ShopDescription';
+import ShopDescription from '../../components/MainPage/ShopDescription/ShopDescription';
+import ShopFeatures from '../../components/MainPage/ShopFeatures/ShopFeatures';
 
 function MainPage() {
-  return <ShopDescription />;
+  return (
+    <main>
+      <ShopDescription />
+      <ShopFeatures />
+    </main>
+  );
 }
 
 export default MainPage;
