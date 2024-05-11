b92b7cb1c84970438738be4d8cba5a2c2a0831e4
HNY-Badger <101455159+HNY-Badger@users.noreply.github.com>
2024-05-11 18:08:22 +0400

    Implement CheckBox component (#16)
    * feat: add CheckBox component, refactor Input
    
    * refactor: change props name

---
+++ src/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,30 @@
+import React, { ChangeEventHandler } from 'react';
+import * as styles from './CheckBox.module.css';
+
+type Props = {
+  checked: boolean;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  label: string;
+  id: string;
+  disabled?: boolean;
+};
+
+function CheckBox({ checked, onChange, label, id, disabled }: Props) {
+  return (
+    <div className={styles.checkbox_wrapper}>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+        id={id}
+        className={styles.checkbox}
+        disabled={disabled}
+      />
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default CheckBox;
