a35fd9f687f24038c7dd91a53a296973f98042c3
HNY_Badger <markelov-02@list.ru>
2024-05-12 22:14:26 +0400

    refactor: remove unnecessary function wrap for store

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import { setupStore } from './store/store';
-
-const store = setupStore();
+import { store } from './store/store';
 
 const root = ReactDom.createRoot(document.getElementById('root')!);
 root.render(
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,11 +5,10 @@ const rootReducer = combineReducers({
   addressesReducer,
 });
 
-export const setupStore = () =>
-  configureStore({
-    reducer: rootReducer,
-  });
+export const store = configureStore({
+  reducer: rootReducer,
+});
 
+export type AppStore = typeof store;
 export type RootState = ReturnType<typeof rootReducer>;
-export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore['dispatch'];
