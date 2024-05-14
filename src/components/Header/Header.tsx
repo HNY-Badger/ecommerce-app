7aba04e41a356ea38c2f240e2425cc54f3443a11
marinrika <marinrika2016@gmail.com>
2024-05-14 20:44:49 +0300

    refactor: refactor class active for NavLink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,17 @@ import { NavLink } from 'react-router-dom';
 import * as style from './Header.module.css';
 
 function Header() {
+  const linkStyle = ({ isActive }: { isActive: boolean }) => (isActive ? style.link_active : style.link);
   return (
     <header className={style.header}>
       <nav className={style.nav}>
-        <NavLink to="/" className={style.link}>
+        <NavLink to="/" end className={linkStyle}>
           Home
         </NavLink>
-        <NavLink to="login" className={style.link}>
+        <NavLink to="login" className={linkStyle}>
           Login
         </NavLink>
-        <NavLink to="registration" className={style.link}>
+        <NavLink to="registration" className={linkStyle}>
           Registration
         </NavLink>
       </nav>
