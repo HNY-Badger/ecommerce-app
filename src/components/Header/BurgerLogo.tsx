878a23d1c773f3c2244387ff46ddd552ba4fbd5f
marinrika <marinrika2016@gmail.com>
2024-05-17 17:18:10 +0300

    feat: add burger menu

---
+++ src/components/Header/BurgerLogo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as style from './BurgerLogo.module.css';
+
+type Props = {
+  isOpen: boolean;
+  onClick: () => void;
+};
+
+function BurgerLogo({ isOpen, onClick }: Props) {
+  return (
+    <button className={style.burger} onClick={onClick} type="button" aria-label="BurgerLogo">
+      <span className={isOpen ? `${style.line} ${style.top_open}` : style.line} />
+      <span className={isOpen ? `${style.line} ${style.middle_open}` : style.line} />
+      <span className={isOpen ? `${style.line} ${style.bottom_open}` : style.line} />
+    </button>
+  );
+}
+
+export default BurgerLogo;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import * as style from './Header.module.css';
+import BurgerLogo from './BurgerLogo';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenuHandler = () => {
+    setIsMenuOpen((prev: boolean) => !prev);
+  };
+
   const linkStyle = ({ isActive }: { isActive: boolean }) => (isActive ? style.link_active : style.link);
+
   return (
     <header className={style.header}>
-      <nav className={style.nav}>
-        <NavLink to="/" end className={linkStyle}>
+      <BurgerLogo isOpen={isMenuOpen} onClick={toggleMenuHandler} />
+      <nav className={isMenuOpen ? `${style.nav} ${style.nav_open}` : style.nav}>
+        <NavLink to="/" end className={linkStyle} onClick={() => setIsMenuOpen(false)}>
           Home
         </NavLink>
-        <NavLink to="login" className={linkStyle}>
+        <NavLink to="login" className={linkStyle} onClick={() => setIsMenuOpen(false)}>
           Login
         </NavLink>
-        <NavLink to="registration" className={linkStyle}>
+        <NavLink to="registration" className={linkStyle} onClick={() => setIsMenuOpen(false)}>
           Registration
         </NavLink>
       </nav>
