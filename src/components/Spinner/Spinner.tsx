dc675a06e3e95ac458b33d7728cd2289a5aaf42f
HNY-Badger <101455159+HNY-Badger@users.noreply.github.com>
2024-05-18 12:06:38 +0400

    feat: add spinner (#32)

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,15 @@
+import React, { CSSProperties } from 'react';
+import * as styles from './Spinner.module.css';
+
+function Spinner(style: CSSProperties) {
+  return (
+    <div className={styles.spinner} style={style}>
+      <div className={styles.part1} />
+      <div className={styles.part2} />
+      <div className={styles.part3} />
+      <div className={styles.part4} />
+    </div>
+  );
+}
+
+export default Spinner;
