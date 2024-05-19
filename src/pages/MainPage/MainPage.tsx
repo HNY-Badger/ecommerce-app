5267bb5b9d782bf017638436b8456ae6c79d1ed5
HNY-Badger <101455159+HNY-Badger@users.noreply.github.com>
2024-05-19 15:20:15 +0400

    feat: add active pages section (#39)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ShopDescription from '../../components/MainPage/ShopDescription/ShopDescription';
 import ShopFeatures from '../../components/MainPage/ShopFeatures/ShopFeatures';
+import ActivePages from '../../components/MainPage/ActivePages/ActivePages';
 
 function MainPage() {
   return (
     <main>
       <ShopDescription />
+      <ActivePages />
       <ShopFeatures />
     </main>
   );
