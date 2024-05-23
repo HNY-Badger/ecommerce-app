b08c375cb9635f8ce2d61c1ef3ac1c899cca009f
HNY_Badger <markelov-02@list.ru>
2024-05-24 01:15:04 +0400

    feat: add products slice

---
+++ src/store/async/ProductsThunk.ts
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AxiosError } from 'axios';
+import { Product, ProductsParams } from '../../types/products';
+import ProductsAPI from '../../api/products';
+import { APIErrorResponse } from '../../types/api';
+
+const fetchProducts = createAsyncThunk<Product[] | undefined, ProductsParams, { rejectValue: string | undefined }>(
+  'products/fetchProducts',
+  async (params: ProductsParams, thunkAPI) => {
+    try {
+      const resp = await ProductsAPI.getProducts(params);
+      return resp;
+    } catch (e) {
+      const err = e as AxiosError<APIErrorResponse>;
+      thunkAPI.rejectWithValue(err.response?.data.message);
+      return undefined;
+    }
+  }
+);
+
+export default fetchProducts;
