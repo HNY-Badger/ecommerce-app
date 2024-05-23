ad5e4738b8a942f21a9d30328b3dad13c0783422
HNY-Badger <101455159+HNY-Badger@users.noreply.github.com>
2024-05-23 22:38:11 +0400

    Implement products fetch (#64)
    * refactor: put interceptor in dedicated file
    
    * feat: add products types and data parsers
    
    * feat: add filter and sort builder according to API docs
    
    * feat: add api for products
    
    * refactor: remove 'key' property

---
+++ src/types/products.ts
@@ -0,0 +1,119 @@
+type Response<T> = {
+  count: number;
+  limit: number;
+  offset: number;
+  results: T;
+};
+
+// Categories
+export type CategoryResult = {
+  id: string;
+  name: {
+    ['en-US']: string;
+  };
+  parent?: {
+    id: string;
+  };
+};
+
+export type CategoriesResponse = Response<CategoryResult[]>;
+
+export type Subcategory = {
+  id: string;
+  name: string;
+  parent: string;
+};
+
+export type Category = {
+  id: string;
+  name: string;
+  subcategories: Subcategory[];
+};
+
+// Products
+export type ProductsParams = {
+  ['text.en-US']?: string;
+  limit?: number;
+  offset?: number;
+  filter?: string[];
+  sort?: string;
+};
+
+export type ProductCategory = {
+  id: string;
+};
+
+export type ProductAttribute = {
+  name: string;
+  value: string;
+};
+
+export type ProductImage = {
+  url: string;
+};
+
+export type ProductPrice = {
+  country: string;
+  value: {
+    centAmount: number;
+    currencyCode: string;
+  };
+  discounted?: {
+    value: {
+      centAmount: number;
+      currencyCode: string;
+    };
+  };
+};
+
+export type ProductResult = {
+  id: string;
+  name: {
+    ['en-US']: string;
+  };
+  description: {
+    ['en-US']: string;
+  };
+  categories: ProductCategory[];
+  masterVariant: {
+    attributes: ProductAttribute[];
+    images: ProductImage[];
+    prices: ProductPrice[];
+  };
+};
+
+export type ProductsResponse = Response<ProductResult[]>;
+
+export type DetailedProductResult = {
+  id: string;
+  masterData: {
+    current: {
+      name: {
+        ['en-US']: string;
+      };
+      description: {
+        ['en-US']: string;
+      };
+      categories: ProductCategory[];
+      masterVariant: {
+        attributes: ProductAttribute[];
+        images: ProductImage[];
+        prices: ProductPrice[];
+      };
+    };
+  };
+};
+
+export type Product = {
+  id: string;
+  categoryId: string;
+  name: string;
+  description: string;
+  attributes: ProductAttribute[];
+  images: ProductImage[];
+  price: {
+    centValue: number;
+    currencyCode: string;
+    nonDiscountCentValue?: number;
+  };
+};
