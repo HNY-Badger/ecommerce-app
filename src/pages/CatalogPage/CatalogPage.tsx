8d6da116c262c9dcd97c8451757b90761cdb71a2
HNY_Badger <markelov-02@list.ru>
2024-05-29 01:30:04 +0400

    feat: add Search and Sort to the catalog page

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,19 +1,65 @@
-import React, { useEffect } from 'react';
+import React, { ChangeEventHandler, useEffect, useState } from 'react';
 import * as styles from './CatalogPage.module.css';
 import Products from '../../components/CatalogPage/Products/Products';
 import fetchProducts from '../../store/async/ProductsThunk';
 import { useAppDispatch } from '../../store/hooks/redux';
+import Search from '../../components/CatalogPage/Search/Search';
+import { ProductsParams } from '../../types/products';
+import Sort from '../../components/CatalogPage/Sort/Sort';
+import ProductParamBuilder from '../../data/Products/paramBuilder';
+
+function formatSortType(sortType: string): string {
+  switch (sortType) {
+    case 'name-asc':
+      return ProductParamBuilder.sort.format('name', 'asc');
+    case 'name-desc':
+      return ProductParamBuilder.sort.format('name', 'desc');
+    case 'price-asc':
+      return ProductParamBuilder.sort.format('price', 'asc');
+    case 'price-desc':
+      return ProductParamBuilder.sort.format('price', 'desc');
+    default:
+      return ProductParamBuilder.sort.format('name', 'asc');
+  }
+}
 
 function CatalogPage() {
   const dispatch = useAppDispatch();
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [sortType, setSortType] = useState<string>('name-asc');
+
+  const fetchHandler = (prodParams: ProductsParams) => {
+    const params: ProductsParams = { ...prodParams };
+    if (searchQuery.length > 0) params['text.en-US'] = searchQuery;
+    if (!prodParams.sort) params.sort = formatSortType(sortType);
+    dispatch(fetchProducts(params));
+  };
 
   useEffect(() => {
-    dispatch(fetchProducts({}));
+    fetchHandler({});
   }, []);
+
+  const sortHandler: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setSortType(e.target.value);
+    fetchHandler({ sort: formatSortType(e.target.value) });
+  };
 
   return (
     <div className={styles.catalog}>
-      <Products />
+      <div className={styles.content}>
+        <div className={styles.sidebar}>Placeholder</div>
+        <div className={styles.main}>
+          <div className={styles.top_bar}>
+            <Search
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onSubmit={() => fetchHandler({})}
+            />
+            <Sort value={sortType} onChange={sortHandler} />
+          </div>
+          <Products />
+        </div>
+      </div>
     </div>
   );
 }
