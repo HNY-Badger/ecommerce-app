90f3f10117d5e3b1dc27d9803587c7544b6320dc
HNY_Badger <markelov-02@list.ru>
2024-06-05 03:20:10 +0400

    refactor: improve pagination to handle more pages

--- src/components/CatalogPage/Pagination/Pagination.tsx
+++ src/components/CatalogPage/Pagination/Pagination.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from 'react-icons/fa';
 import * as styles from './Pagination.module.css';
 
 type Props = {
@@ -8,20 +9,58 @@ type Props = {
 };
 
 function Pagination({ currentPage, onPageChange, totalPages }: Props) {
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  let pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  if (totalPages > 7) {
+    const minOffset = currentPage - 2;
+    const min = minOffset < 0 ? 0 : minOffset;
+    const maxOffset = currentPage + 3;
+    const max = maxOffset > totalPages - 1 ? totalPages : maxOffset;
+    pages = pages.slice(min, max);
+  }
 
   return (
     <div className={styles.pages}>
-      {pages.map((page, index) => (
-        <button
-          type="button"
-          className={`${styles.page} ${index === currentPage ? styles.active : ''}`}
-          onClick={() => onPageChange(index)}
-          key={page}
-        >
-          {page}
-        </button>
-      ))}
+      {totalPages > 7 ? (
+        <>
+          {pages[0] !== 1 && (
+            <>
+              <button type="button" className={`${styles.page}`} onClick={() => onPageChange(0)}>
+                1
+              </button>
+              <FaAngleDoubleLeft />
+            </>
+          )}
+          {pages.map((page) => (
+            <button
+              type="button"
+              className={`${styles.page} ${page - 1 === currentPage ? styles.active : ''}`}
+              onClick={() => onPageChange(page - 1)}
+              key={page}
+            >
+              {page}
+            </button>
+          ))}
+          {pages[pages.length - 1] !== totalPages && (
+            <>
+              <FaAngleDoubleRight />
+              <button type="button" className={`${styles.page}`} onClick={() => onPageChange(totalPages - 1)}>
+                {totalPages}
+              </button>
+            </>
+          )}
+        </>
+      ) : (
+        pages.map((page) => (
+          <button
+            type="button"
+            className={`${styles.page} ${page - 1 === currentPage ? styles.active : ''}`}
+            onClick={() => onPageChange(page - 1)}
+            key={page}
+          >
+            {page}
+          </button>
+        ))
+      )}
     </div>
   );
 }
