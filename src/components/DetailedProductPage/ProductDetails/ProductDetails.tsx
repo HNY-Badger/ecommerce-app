3f73ed95033d2a09ce6040469cb773c118633cc6
HNY-Badger <101455159+HNY-Badger@users.noreply.github.com>
2024-06-05 21:32:18 +0400

    Enhance detailed page (#110)
    * feat: enhance detailed page
    
    * fix: image slider was over burger menu

--- src/components/DetailedProductPage/ProductDetails/ProductDetails.tsx
+++ src/components/DetailedProductPage/ProductDetails/ProductDetails.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as styles from './ProductDetails.module.css';
 import { Product } from '../../../types/products';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks/redux';
+import { refreshCart, updateCart } from '../../../store/async/CartThunk';
+import Button from '../../Button/Button';
+import Counter from '../../Counter/Counter';
 
 function formatPrice(price: number, currencyCode: string): string {
   return Intl.NumberFormat('en-US', { style: 'currency', currency: currencyCode }).format(price);
@@ -11,6 +15,62 @@ type Props = {
 };
 
 function ProductDetails({ product }: Props) {
+  const dispatch = useAppDispatch();
+  const { data: cart, loading, error } = useAppSelector((state) => state.cartReducer);
+  const productInCart = cart?.lineItems.find((item) => item.productId === product.id);
+
+  useEffect(() => {
+    if ((!cart && !loading) || error.length > 0) {
+      dispatch(refreshCart());
+    }
+  }, [cart, loading, error]);
+
+  const addButtonClickHandler = () => {
+    if (cart !== null) {
+      dispatch(
+        updateCart.addLineItem({
+          id: cart.id,
+          version: cart.version,
+          actionBody: { productId: product.id, quantity: 1 },
+        })
+      );
+    }
+  };
+
+  const removeButtonClickHandler = () => {
+    if (cart !== null && productInCart) {
+      dispatch(
+        updateCart.removeLineItem({
+          id: cart.id,
+          version: cart.version,
+          actionBody: { lineItemId: productInCart.id, quantity: productInCart.quantity },
+        })
+      );
+    }
+  };
+
+  const quantityHandler = (quantity: number) => {
+    if (cart !== null && productInCart) {
+      if (quantity <= 0) {
+        dispatch(
+          updateCart.removeLineItem({
+            id: cart.id,
+            version: cart.version,
+            actionBody: { lineItemId: productInCart.id, quantity: productInCart.quantity },
+          })
+        );
+      } else {
+        dispatch(
+          updateCart.changeLineItemQuantity({
+            id: cart.id,
+            version: cart.version,
+            actionBody: { lineItemId: productInCart.id, quantity },
+          })
+        );
+      }
+    }
+  };
+
   return (
     <div className={styles.info}>
       <div className={styles.heading}>
@@ -41,6 +101,16 @@ function ProductDetails({ product }: Props) {
           ))}
         </ul>
       </div>
+      {productInCart ? (
+        <>
+          <Button variant="remove" onClick={removeButtonClickHandler}>
+            Remove from the cart
+          </Button>
+          <Counter value={productInCart.quantity} onChange={quantityHandler} />
+        </>
+      ) : (
+        <Button onClick={addButtonClickHandler}>Add to cart</Button>
+      )}
     </div>
   );
 }
